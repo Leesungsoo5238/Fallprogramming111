496f78daaf4ed14787f38e6685ac7ebe4fb29529
Leesungsoo5238 <sungsoo5238@naver.com>
2020-09-15 01:40:00 +0900

    report2 porb.1-c No.3

---
+++ report2 prob.1-C no.3/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int kor = 70, eng = 80, mat = 90;
+  int tot;
+
+  tot = kor + eng + mat;
+  printf("kor : %d, eng : %d, mat : %d/n", kor, eng, mat);
+  printf("tot : %d", tot);
+
+  return 0;
+}
